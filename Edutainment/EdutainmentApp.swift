c9f6ac7c262246ed6a91cd31e1d0038ce755b18b
marnierogers <marnierogers1@gmail.com>
2023-07-15 09:38:02 +1000

    Initial Commit

---
+++ Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by Marnie Rogers on 15/7/2023.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
